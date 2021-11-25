41ad7257f423a6c3f86edc5989cbdb2a63529d21
Leshan Patterson <leshan0627@gmail.com>
2021-11-25 01:45:32 +0000

    eqObjects.js update

--- eqObjects.js
+++ eqObjects.js
@@ -1,18 +1,30 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   } for (let keys of Object.keys(object1)) {
-    if (object1[keys] !== object2[keys]) {
+    if (Array.isArray(object1[keys]) === true && Array.isArray(object2[keys]) === true) {
+      eqArrays(object1, object2);
+    } else if (object1[keys] !== object2[keys]) {
       return false;
-    }
-  } return true;
+    } return true;
+  }
 };
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
